6a3fa4f22282642107f8290c9433221a8b74ac35
rusty <rusty3031@gmail.com>
2022-07-04 22:16:58 -0700

    testing

---
+++ public/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <!-- ICON  -->
+    <link rel="shortcut icon" href="./img/icon-rusty.png" type="image/x-icon">
+    <!-- css -->
+    <link rel="stylesheet" href="./css/index.css">
+    <script src="https://kit.fontawesome.com/0f2756a855.js" crossorigin="anonymous"></script>
+    <title>Marcia Servin</title>
+</head>
+<body>
+        <!-- <nav class="navigator">
+            <ul class="list-items">
+                <button class="darkmode">DarkMode</button>
+            </ul>
+        </nav> -->
+
+        <!-- profile -->
+        <!-- <div class="profile-content">
+            <img src="" alt="">
+        </div> -->
+
+        <!-- content -->
+        <div class="content">
+            <h1 id="title" class="bigtext">Marcia Servin<br><span class="small">Artista y tatuadora</span></h1> 
+        </div>
+        <div class="category">
+            <ul class="category-list">
+                <li id="all-section">Todo</li>
+                <li id="tech-section">Tecnologia</li>
+                <li id="ideas">Ideas</li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="tech-box">
+                <img src="./img/tech/img-1.jpg" alt="img" id="img-tech-1">
+            </div>  
+            <div class="tech-box">
+                <img src="./img/tech/img-2.jpg" alt="img" id="img-tech-2">
+            </div>  
+            <div class="tech-box">
+                <img src="./img/tech/img-3.jpg" alt="img" id="img-tech-3">
+            </div>  
+            <div class="tech-box">
+                <img src="./img/tech/img-4.jpg" alt="img">
+            </div>      
+            <div class="tech-box">
+                <img src="./img/tech/luna-natural.jpg" alt="img" id="ideas-image">
+            </div>      
+            <div class="tech-box">
+                <img src="./img/tech/luna2-natural.jpg" alt="img">
+            </div>      
+        </div>
+
+    <!-- scripts -->
+    <!-- <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script> -->
+    <script src="../model/app.js"></script>
+</body>
+</html>
